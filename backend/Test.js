9ae3d6009174f509f168dfb720249b1fc6e91b3c
akm-islam <mamunjahir.com@gmail.com>
2021-06-21 21:06:35 -0400

    ra

--- backend/Test.js
+++ backend/Test.js
@@ -4,8 +4,8 @@ var url = 'mongodb://akm:Ma%40142566@3.140.216.86:27017/cool_db';
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("cool_db");
-  var query = { Month: "March",scoresSubjectivity: 0 };
-  dbo.collection("march").find(query).toArray(function(err, result) {
+  var query = { FlairSentiment: "negative",scoresSubjectivity: 0 };
+  dbo.collection("April").find({index : {$lt : 100}}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
